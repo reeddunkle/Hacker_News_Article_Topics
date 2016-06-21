b79a496b88d93960ffae04453d15af6220d7e45d
Reed Dunkle <reeddunkle@gmail.com>
2016-06-20 22:58:37 -0400

    Added arg parsing to devel branch.

--- newscomb/hn_scrape.py
+++ newscomb/hn_scrape.py
@@ -5,6 +5,7 @@
     Scrape and store Hacker News articles.
 """
 
+import argparse
 import requests
 import pandas as pd
 from exceptions import TypeError, RuntimeError
@@ -14,27 +15,35 @@ from exceptions import TypeError, RuntimeError
 ROOT_URL = 'https://hacker-news.firebaseio.com/v0/'
 
 
-def scrape_HN_articles_write_to_csv():
+def scrape_HN_articles_write_to_csv(count=0):
     '''
     Scrapes top stories on Hacker News, creates dataframe with content,
     and writes it to a .csv file.
     '''
+    if count > 0:
+        limit = int(count)
+    else:
+        limit = None
+
+    print("Limit is set to: {}".format(limit))
 
     top_stories = requests.get(ROOT_URL + 'topstories.json').json()
+
+
     articles = []
 
-    for article_id in top_stories:
+    for article_id in top_stories[:limit]:
         article = requests.get(ROOT_URL + "item/" + str(article_id) + ".json").json()
-        print article  # For debugging
+        article_type = article['type']
 
-        if 'url' in article:
+        if 'url' in article and article_type == 'story':
             article_url = article['url']
             try:
                 article_page = requests.get(article_url)
                 content = article_page.content.decode('utf-8', 'ignore')
 
                 article_row = [article['by'], article['descendants'], article['id'], article['score'],
-                     article['time'], article['title'], article['type'], article_url, content]
+                     article['time'], article['title'], article_type, article_url, content]
 
                 articles.append(article_row)
 
@@ -45,8 +54,22 @@ def scrape_HN_articles_write_to_csv():
                print('wrong password')
 
     df = pd.DataFrame.from_records(articles, columns=['by', 'descendants', 'id', 'score', 'time', 'title', 'type', 'url', 'html'])
-    df.to_csv("../data/articles.csv", encoding="utf-8", index=False)
+    df.to_csv("data/articles.csv", encoding="utf-8", index=False)
+
+
+def gen_parser():
+
+    parser = argparse.ArgumentParser(description='Manipulate an image.')
+    parser.add_argument('--count', dest='count', required=False, nargs='?', default=0, type=str, help='Set the number of top articles to scrape. If none given, will scrape all')
+
+    return parser
 
 
 if __name__ == '__main__':
-    scrape_HN_articles_write_to_csv()
+
+    parser = gen_parser()
+    args = parser.parse_args()
+    count = args.count
+
+    if count.isdigit() and count >= 0:
+        scrape_HN_articles_write_to_csv(count)
