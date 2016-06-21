822c8c5698de23b6eb7a8e53aef63664ea64ce2a
Reed Dunkle <reeddunkle@gmail.com>
2016-06-20 22:56:36 -0400

    Constructing LDA topic modeling.

--- newscomb/topic_modeling.py
+++ newscomb/topic_modeling.py
@@ -9,8 +9,9 @@ from stop_words import get_stop_words
 from gensim import corpora, models
 from goose import Goose
 from nltk.collocations import BigramCollocationFinder
+from sklearn.feature_extraction.text import CountVectorizer
 import nltk
-import numpy
+import numpy as np
 import gensim
 import lda
 import pandas
@@ -133,12 +134,23 @@ def create_document_term_matrix(texts):
     return corpus
 
 
+def display_LDA_topics(topic_word):
+    '''
+    Given list of topic words from LDA model, PRINTS topic words for topic (no return)
+    '''
+
+    n_top_words = 34
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+
+
 if __name__ == '__main__':
 
-    dataframe = pandas.read_csv("../data/articles.csv", index_col=False)
+    dataframe = pandas.read_csv("data/articles.csv", index_col=False)
 
     html_list = dataframe['html']
-    articles_list = extract_title_and_text_from_html(html_list[:20]) # 20 for testing
+    articles_list = extract_title_and_text_from_html(html_list)
 
     tuple_of_lists = transpose_tuples_lists(articles_list)
     title_list, text_list = tuple_of_lists
@@ -150,12 +162,35 @@ if __name__ == '__main__':
     articles = (clean_title_list, collocation_ready_text)
     display_collocations(articles)
 
+    """--------------------------------------------------"""
+
+    texts_for_LDA = normalize_all_texts_for_lda(text_list)
+
+    # cv = CountVectorizer()
+    # cv.fit(texts_for_LDA)
+
+    # max_df excludes any word that appears more than x * 100 % of the time
+    # min_df requires a word to appear at least y times
+    cv = CountVectorizer(max_df=.4, min_df=3)
+    mat = cv.fit_transform([' '.join(x) for x in texts_for_LDA])
+    mat.shape
+    model = lda.LDA(n_topics=8, n_iter=400)
+    model.fit(mat)
+
+    vocab = [x[0] for x in sorted(cv.vocabulary_.iteritems(), key=lambda t: t[1])]
+    topic_word = model.topic_word_
+
+
+
+
     """
     ----------------------------
     FOR LATER LDA IMPLEMENTATION
     """
 
-    # texts_for_LDA = normalize_all_texts_for_lda(texts_clean)
+
+
+
 
     # vocab = sum(texts_for_LDA, [])
     # titles = title_list
@@ -169,7 +204,7 @@ if __name__ == '__main__':
     # n_top_words = 8
 
     # for i, topic_dist in enumerate(topic_word):
-    #     topic_words = numpy.array(vocab)[numpy.argsort(topic_dist)][:-(n_top_words+1):-1]
+    #     topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
     #     print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
     # articles_for_lda = dict(zip(title_list, texts_for_LDA))
