1f9a29febbed6cbb4beb2614b367bc188f65bf3d
Reed Dunkle <reeddunkle@gmail.com>
2016-06-23 14:49:09 -0400

    Removed debug print.

--- newscomb/word_processing.py
+++ newscomb/word_processing.py
@@ -47,7 +47,6 @@ def generate_collocations(tokens):
     bigram_measures = nltk.collocations.BigramAssocMeasures()
 
     # Best results with window_size, freq_filter of: (2,1) (2,2) (5,1)
-    # print(tokens)
     finder = BigramCollocationFinder.from_words(tokens, window_size = 2)
     finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
     finder.apply_freq_filter(1)
