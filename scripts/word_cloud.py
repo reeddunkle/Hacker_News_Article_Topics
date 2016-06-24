2002ca00f0c5ac755474921743cfa0b6de7ef0c3
Reed Dunkle <reeddunkle@gmail.com>
2016-06-24 10:23:46 -0400

    Added word_cloud.py

---
+++ scripts/word_cloud.py
@@ -0,0 +1,32 @@
+"""
+    word_cloud
+    ~~~~~~~~~~
+
+    Generates a word cloud from data/article.csv
+"""
+
+
+
+from wordcloud import WordCloud
+from newscomb.text_cleanup import normalize_articles_for_collocation
+from newscomb.word_processing import display_collocations
+from newscomb.extract import extract_titles_text_and_urls
+
+
+
+if __name__ == '__main__':
+
+    articles_list = extract_titles_text_and_urls()
+    collocation_ready_articles = normalize_articles_for_collocation(articles_list)
+    titles_tuples, text_tuples, urls_tuples = zip(*collocation_ready_articles)
+
+    titles_list = list(titles_tuples)
+    text_list = list(text_list)
+
+
+    text = ' '.join(sum(text_list, []))
+
+    cloud = WordCloud().generate(text)
+
+    image = cloud.to_image()
+    image.show()
